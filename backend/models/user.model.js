6c0103094904d5101628341b01a4bcc43e51a340
empero6 <empero6@gmail.com>
2020-05-03 22:09:11 -0400

    Add files via upload

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -6,10 +6,73 @@ const userSchema = new Schema({
     firstName: {
         type: String,
         required: true,
-        unique: true,
         trim: true,
-        minlenth: 3
+        minlength: 3
     },
+
+    campus: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+
+    pantherId: {
+        type: Number,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+    
+
+    department: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+
+    level: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+
+    degree: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+    
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+
+    college: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+    
+    year: {
+        type: Number,
+        required: true,
+        trim: true,
+        minlength: 3
+    },  
 }, {
     timestamps: true,
 });
