138112c1bb5821d11da847a6a466cf03b91da1ca
empero6 <empero6@gmail.com>
2020-05-03 22:09:36 -0400

    Add files via upload

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,8 +9,28 @@ router.route('/').get((req,res) => {
 
 router.route('/add').post((req, res) => {
     const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const pantherId = req.body.pantherId;
+    const department = req.body.department;
+    const level = req.body.level;
+    const campus = req.body.campus;
+    const degree = req.body.degree;
+    const email = req.body.email;
+    const college = req.body.college;
+    const year = req.body.year;
 
-    const newUser = new User({firstName});
+    const newUser = new User({
+        firstName,
+        lastName,
+        pantherId,
+        department,
+        level,
+        campus,
+        degree,
+        email,
+        college,
+        year,
+    });
 
     newUser.save()
         .then(() => res.json('User added!'))
